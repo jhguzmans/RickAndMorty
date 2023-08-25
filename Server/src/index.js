d40e4732599caef171ea4e629100ce5d516c43fd
Julian Guzman <jhguzmans@unal.edu.co>
2023-08-24 19:38:51 -0500

    Previo al CP del M4

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,7 @@
 // const http = require("http");
 // const { log } = require("console");
 // const { getCharById } = require("./controllers/getCharById");
+const { conn } = require("./DB_connection");
 const express = require("express");
 const server = express();
 const PORT = 3001;
